ab3850cb44fe0862b17a61001565837aa953e6e1
zalox <jorgen@guut.org>
2013-04-23 08:20:47 +0200

    Added documentation for customer.

--- procatering/Customer.java
+++ procatering/Customer.java
@@ -29,27 +29,33 @@ public class Customer extends Person{
         customerID = cid;
     }
 	/**
-	 * @author Team17
-	 * //TODO legg til dokumentasjon
+	 * Regular get method for address.
+	 * Returns a string value of the address.
 	 */
     public String getAddress() {
         return address;
     }
 	/**
-	 * @author Team17
-	 * //TODO legg til dokumentasjon
+	 * Regular set method for the address
+	 * Assigns a new string value to the address.
 	 */
     public void setAddress(String address) {
         this.address = address;
     }
 
+	/**
+	 * Method findCustomer returns a DefaltListModel containing all the customers in the database matching search criteria.
+	 * @param search
+	 * @return a DefaultListModel<Customer> object.
+	 */
 	public static DefaultListModel<Customer> findCustomer(String search){
 		Database db = new Database();
 		return db.findCustomer(search);
 	}
-
-
     @Override
+	/**
+	 * toString returns a string representation of the customer object.
+	 */
     public String toString() {
         return getLastName()+", "+getFirstName();
     }
