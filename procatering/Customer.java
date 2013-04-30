8586f702cad512f88a584cd0333f643199a6bce7
Zalox <jorgen@guut.org>
2013-04-30 22:33:03 +0200

    Write documentation for customer and person, also add equals method in Person.

--- procatering/Customer.java
+++ procatering/Customer.java
@@ -5,131 +5,187 @@ import database.Database;
 import javax.swing.*;
 
 /**
- * @author Ted
+ * Customer class contains all the information about the customer. It also opens for some queries towards the database.
+ *
+ * @author Ted Kristoffersen
  */
-public class Customer extends Person{
+public class Customer extends Person {
     private String address;
-	private int customerID;
+    private int customerID;
     private int corporateNum;
     private String corporateName;
-	private String note;
+    private String note;
 
-	/**
-	 * This constructor should me used when inserting a customer in the database.
-	 *
-	 * @param adr The customers address
-	 * @param fn The customers first name
-	 * @param ln The customers last name
-	 * @param phone The customers phone number
-	 * @param mail The customers email
-	 * @param pCode The customers postal code
-	 * @param note The customers special note
-	 * @author Jørgen Lien Sellæg
-	 */
-    public Customer(String adr, String fn, String ln, String phone, String mail, int pCode, String note){
-        super(fn,  ln,  phone,  mail, pCode);
+    /**
+     * This constructor should me used when inserting a customer in the database.
+     *
+     * @param adr   The customers address
+     * @param fn    The customers first name
+     * @param ln    The customers last name
+     * @param phone The customers phone number
+     * @param mail  The customers email
+     * @param pCode The customers postal code
+     * @param note  The customers special note
+     * @author Jørgen Lien Sellæg
+     */
+    public Customer(String adr, String fn, String ln, String phone, String mail, int pCode, String note) {
+        super(fn, ln, phone, mail, pCode);
         this.address = adr;
-		this.note = note;
+        this.note = note;
     }
 
-	/**
-	 * This constructor should me used when extracting a customer from the database.
-	 *
-	 * @param adr The customers address
-	 * @param fn The customers first name
-	 * @param ln The customers last name
-	 * @param phone The customers phone number
-	 * @param mail The customers email
-	 * @param pCode The customers postal code
-	 * @param note The customers special note
-	 * @param cid The customers id in the database.
-	 *
-	 * @author Jørgen Lien Sellæg
-	 */
-    public Customer(String adr, String fn, String ln, String phone, String mail, int pCode, String note, int cid){
-        super( fn,  ln,  phone,  mail, pCode);
+    /**
+     * This constructor should me used when extracting a customer from the database.
+     *
+     * @param adr   The customers address
+     * @param fn    The customers first name
+     * @param ln    The customers last name
+     * @param phone The customers phone number
+     * @param mail  The customers email
+     * @param pCode The customers postal code
+     * @param note  The customers special note
+     * @param cid   The customers id in the database.
+     * @author Jørgen Lien Sellæg
+     */
+    public Customer(String adr, String fn, String ln, String phone, String mail, int pCode, String note, int cid) {
+        super(fn, ln, phone, mail, pCode);
         this.address = adr;
-		this.note = note;
-		customerID = cid;
+        this.note = note;
+        customerID = cid;
     }
-	/** Standard copy constructor */
-	public Customer(Customer customer){
-		super(customer.getFirstName(), customer.getLastName(), customer.getPhoneNumber(), customer.getEmail(), customer.getPostalCode());
-		this.address = customer.getAddress();
-		this.note = customer.getNote();
-		this.customerID = customer.getCustomerID();
-	}
-	/**
-	 * Regular get method for address.
-	 * Returns a string value of the address.
-	 */
+
+    /**
+     * Standard copy constructor. Should be used when it is needed a new reference to the object in memory.
+     *
+     * @param customer the customer object to be copied.
+     */
+    public Customer(Customer customer) {
+        super(customer.getFirstName(), customer.getLastName(), customer.getPhoneNumber(), customer.getEmail(), customer.getPostalCode());
+        this.address = customer.getAddress();
+        this.note = customer.getNote();
+        this.customerID = customer.getCustomerID();
+    }
+
+    /**
+     * Method findCustomer returns a DefaltListModel containing all the customers in the database matching search criteria.
+     *
+     * @param search
+     * @return a DefaultListModel<Customer> object.
+     */
+    public static DefaultListModel<Customer> findCustomer(String search) {
+        Database db = new Database();
+        return db.findCustomer(search);
+    }
+
+    /**
+     * Checks whether the customer exist in the database or not.
+     *
+     * @param customer the customer to be checked against the database.
+     * @return true if the customer already exist in the database, false if the customer is not found. A customer is equal to a different customer if the phone number and first name is the same. The first name is not case sensitive.
+     */
+    public static boolean customerExist(Customer customer) {
+        Database db = new Database();
+        return db.customerExist(customer);
+    }
+
+    /**
+     * Regular get method for address.
+     * Returns a string value of the address.
+     */
     public String getAddress() {
         return address;
     }
-	/**
-	 * Regular set method for the address
-	 * Assigns a new string value to the address.
-	 */
+
+    /**
+     * Regular set method for the address
+     * Assigns a new string value to the address.
+     */
     public void setAddress(String address) {
         this.address = address;
     }
 
-	/**
-	 * Method findCustomer returns a DefaltListModel containing all the customers in the database matching search criteria.
-	 * @param search
-	 * @return a DefaultListModel<Customer> object.
-	 */
-	public static DefaultListModel<Customer> findCustomer(String search){
-		Database db = new Database();
-		return db.findCustomer(search);
-	}
-
+    /**
+     * Gets the corporate number of the customer.
+     *
+     * @return the corporate number of the customer, null if it is not set.
+     */
     public int getCorporateNum() {
         return corporateNum;
     }
 
+    /**
+     * Sets the corporate of the customer.
+     *
+     * @param corporateNum the new corporate number.
+     */
     public void setCorporateNum(int corporateNum) {
         this.corporateNum = corporateNum;
     }
 
+    /**
+     * Gets the corporate name of the customer.
+     *
+     * @return the corporate name of the customer. Null if the field is not set.
+     */
     public String getCorporateName() {
         return corporateName;
     }
-	/* Gets the customer ID of the customer as it is in the Database. */
-	public int getCustomerID() {
-		return customerID;
-	}
 
+    /**
+     * Sets the corporate name of an customer.
+     *
+     * @param corporateName The new cooperate name.
+     */
     public void setCorporateName(String corporateName) {
         this.corporateName = corporateName;
     }
 
-	public String getNote(){
-		return this.note;
-	}
+    /**
+     * Gets the customer ID of the customer as it is in the Database.
+     *
+     * @return the customerID stored in the customer object.
+     */
+    public int getCustomerID() {
+        return customerID;
+    }
 
-	public boolean addCustomer(Customer customer) {
-		Database db = new Database();
-		if(customer != null)
-			return db.addCustomer(customer);
-		return false;
-	}
+    /**
+     * Simple getter for the customer note.
+     *
+     * @return the note stored in the object.
+     */
+    public String getNote() {
+        return this.note;
+    }
 
-	public static boolean customerExist(Customer customer){
-		Database db = new Database();
-		return db.customerExist(customer);
-	}
+    /**
+     * Method adds an customer to the database.
+     *
+     * @param customer the customer to be added to the database.
+     * @return true if the customer is successfully added to the database.
+     */
+    public boolean addCustomer(Customer customer) {
+        Database db = new Database();
+        return customer != null && db.addCustomer(customer);
+    }
 
-	public String getPostPlace(String postCode){
-		return Helper.searchPostalCode(postCode);
-	}
+    /**
+     * Method gets the postal place of the customer. I.E. if the parameter is set to 0001, the return will be OSLO.
+     *
+     * @param postCode The postal code that represent the postal place.
+     * @return the postal place of the code given. Returns null if no postal place is found.
+     */
+    public String getPostPlace(String postCode) {
+        return Helper.searchPostalCode(postCode);
+    }
 
-
-	@Override
-	/**
-	 * toString returns a string representation of the customer object.
-	 */
-	public String toString() {
-		return getLastName()+", "+getFirstName();
-	}
+    /**
+     * toString returns a string representation of the customer object.
+     *
+     * @return the first and last name of the customer. I.E. "Gates, Bill".
+     */
+    @Override
+    public String toString() {
+        return getLastName() + ", " + getFirstName();
+    }
 }
--- procatering/Person.java
+++ procatering/Person.java
@@ -8,105 +8,101 @@ package procatering;
  * @author TEAM 17
  */
 public abstract class Person {
-	private String firstName;
-	private String lastName;
-	private String phoneNumber;
-	private String email;
-	private int postalCode;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String email;
+    private int postalCode;
 
-	/**
-	 * Person constructor except all parameters have been checked and is correct before trying to create an object of person.
-	 * @param fn    String
-	 * @param ln    String
-	 * @param phone String
-	 * @param mail  String
-	 * @param pCode String postal code
-	 */
-	public Person(String fn, String ln, String phone, String mail, int pCode) {
-		firstName = fn;
-		lastName = ln;
-		phoneNumber = phone;
-		email = mail;
-		postalCode = pCode;
-	}
+    /**
+     * Person constructor except all parameters have been checked and is correct before trying to create an object of person.
+     *
+     * @param fn    String
+     * @param ln    String
+     * @param phone String
+     * @param mail  String
+     * @param pCode String postal code
+     */
+    public Person(String fn, String ln, String phone, String mail, int pCode) {
+        firstName = fn;
+        lastName = ln;
+        phoneNumber = phone;
+        email = mail;
+        postalCode = pCode;
+    }
 
-	/**
-	 * firstName are of object type String
-	 * @return firstName
-	 */
-	public String getFirstName() {
-		return firstName;
-	}
+    /**
+     * Method returns the fist name of the Person.
+     *
+     * @return the firstName of the person.
+     */
+    public String getFirstName() {
+        return firstName;
+    }
 
-	/**
-	 * Set firstName variable to equal the parameter firstName
-	 * @param firstName String object
-	 */
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
+    /**
+     * lastName are of object type String
+     *
+     * @return the last name of the Person.
+     */
+    public String getLastName() {
+        return lastName;
+    }
 
-	/**
-	 * lastName are of object type String
-	 * @return lastName
-	 */
-	public String getLastName() {
-		return lastName;
-	}
+    /**
+     * Returns the phone number of the Person.
+     *
+     * @return the phone number of the person.
+     */
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
 
-	/**
-	 * Set lastName variable to equal the parameter lastName
-	 * @param lastName
-	 */
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
+    /**
+     * The persons email.
+     *
+     * @return the persons email.
+     */
+    public String getEmail() {
+        return email;
+    }
 
-	/**
-	 * phoneNumber are of object type String
-	 * @return phoneNumber
-	 */
-	public String getPhoneNumber() {
-		return phoneNumber;
-	}
+    /**
+     * @return returns the postal code
+     */
+    public int getPostalCode() {
+        return postalCode;
+    }
 
-	/**
-	 * set phoneNumber variable to equal the parameter phoneNumber
-	 * @param phoneNumber
-	 */
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
+    /**
+     * Is to be used by the sub classes of this object.
+     * The method checks whether the Persons first name and phone number is the same.
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
 
-	/**
-	 * email are of object type String
-	 * @return email
-	 */
-	public String getEmail() {
-		return email;
-	}
+        Person person = (Person) o;
 
-	/**
-	 * set email variable to equal the parameter email
-	 * @param email
-	 */
-	public void setEmail(String email) {
-		this.email = email;
-	}
+        return firstName.toUpperCase().equals(person.firstName.toUpperCase()) && phoneNumber.equals(person.phoneNumber);
 
-	/**
-	 * @return returns the postal code
-	 */
-	public int getPostalCode() {
-		return postalCode;
-	}
+    }
 
-	/**
-	 * toString is of the type String
-	 * @return class information: <br>DOB, firstName, lastName, phoneNumber, email
-	 */
-	@Override
-	public String toString() {
-		return "Person{ firstName=" + firstName + ", lastName=" + lastName + ", phoneNumber=" + phoneNumber + ", email=" + email + '}';
-	}
+    @Override
+    public int hashCode() {
+        int result = firstName.hashCode();
+        result = 31 * result + phoneNumber.hashCode();
+        return result;
+    }
+
+    /**
+     * Returns a string representation of the object.
+     *
+     * @return class information: <br>DOB, firstName, lastName, phoneNumber, email
+     */
+    @Override
+    public String toString() {
+        return "Person{ firstName=" + firstName + ", lastName=" + lastName + ", phoneNumber=" + phoneNumber + ", email=" + email + '}';
+    }
 }
