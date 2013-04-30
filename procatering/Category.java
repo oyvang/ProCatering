bf321aab90636285268c890db90d04050666f255
Zalox <jorgen@guut.org>
2013-04-30 22:37:42 +0200

    Reformat Category.java

--- procatering/Category.java
+++ procatering/Category.java
@@ -1,50 +1,55 @@
 package procatering;
 
 /**
- * //TODO description
+ * Class category contains the information about the several categories.
+ *
  * @author Geir Morten Larsen
  */
 public class Category {
-    
-    private String name;
-    private int categoryID = -1;
+	private String name;
+	private int categoryID = -1;
 
 	/**
 	 * Creates an object of type Category. This constructor should be used when getting a category from a database
+	 *
 	 * @param categoryID The id of the category, as is in the database.
-	 * @param name The name of the category
+	 * @param name       The name of the category
 	 */
-    public Category (int categoryID,String name){
-        this.name = name;
-        this.categoryID=categoryID;
-    }
+	public Category(int categoryID, String name) {
+		this.name = name;
+		this.categoryID = categoryID;
+	}
 
 	/**
 	 * Creates an object of type Category. This constructor should be used when putting a category in to the database
+	 *
 	 * @param name the name of the category.
 	 */
-	public Category (String name){
+	public Category(String name) {
 		this.name = name;
 	}
 
 	/**
 	 * Method gets the name of the category
+	 *
 	 * @return the category name
 	 */
-    public String getName(){
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
 	/**
 	 * Gets the category id from the database
+	 *
 	 * @return the category id. Returns -1 if the category isn't fetched for the database, but created localy.
 	 */
-    public int getCategoryID() {
-        return categoryID;
-    }
+	public int getCategoryID() {
+		return categoryID;
+	}
 
 	/**
 	 * Method returns a string representation of the object.
+	 *
 	 * @return the name of the category
 	 */
 	@Override
